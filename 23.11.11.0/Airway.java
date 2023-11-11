0ece4d366ce70222ff48cfc2fc0e1890eab9997f
Abdulrahman Dülek <114644911+marcusdulekilius@users.noreply.github.com>
2023-11-11 14:14:56 +0300

    Update and rename havayolu.java to Airway.java

---
+++ 23.11.11.0/Airway.java
@@ -0,0 +1,5 @@
+public class Airway {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
--- 23.11.11.0/havayolu.java
+++
@@ -1,5 +0,0 @@
-public class havayolu {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
