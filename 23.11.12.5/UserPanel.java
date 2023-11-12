ef04565cac7d46c423127082227323ffc4fa9a58
Abdulrahman Dülek <114644911+marcusdulekilius@users.noreply.github.com>
2023-11-12 17:28:52 +0300

    Create UserPanel.java
    Added Back and Forward Buttons.

---
+++ 23.11.12.5/UserPanel.java
@@ -0,0 +1,157 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserPanel extends JFrame {
+    private JPasswordField passwordField;
+    private boolean passwordVisible = false;
+
+    public UserPanel() {
+        setTitle("User Panel Login");
+        setSize(1280, 720);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(1, 2));
+
+        // Sol Panel (Simge ve Ad)
+        JPanel leftPanel = new JPanel(new GridBagLayout());
+        leftPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+        ImageIcon symbolImage = new ImageIcon(System.getProperty("user.home") + "/Downloads/logo.png");
+        Image image = symbolImage.getImage();
+        Image newImage = image.getScaledInstance(screenWidth / 2, screenHeight, Image.SCALE_SMOOTH);
+        symbolImage = new ImageIcon(newImage);
+
+        JLabel symbolLabel = new JLabel(symbolImage);
+        symbolLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        symbolLabel.setVerticalAlignment(SwingConstants.CENTER);
+
+        JLabel nameLabel = new JLabel();
+        nameLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+        nameLabel.setForeground(Color.white);
+
+        // symbolLabel için GridBagConstraints oluştur
+        GridBagConstraints symbolConstraints = new GridBagConstraints();
+        symbolConstraints.gridx = 0;
+        symbolConstraints.gridy = 0;
+        symbolConstraints.fill = GridBagConstraints.BOTH;
+        symbolConstraints.weightx = 2.0;
+        symbolConstraints.weighty = 2.0;
+
+        // nameLabel için GridBagConstraints oluştur
+        GridBagConstraints nameConstraints = new GridBagConstraints();
+        nameConstraints.gridx = 0;
+        nameConstraints.gridy = 1;
+
+        leftPanel.add(symbolLabel, symbolConstraints);
+        leftPanel.add(nameLabel, nameConstraints);
+
+        // Sağ Panel (Kullanıcı Adı, Şifre ve Giriş)
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        rightPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JTextField usernameField = new JTextField(30);
+        usernamePanel.add(new JLabel("Username:"));
+        usernamePanel.add(usernameField);
+
+        passwordField = new JPasswordField(30);
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        passwordPanel.add(new JLabel("Password:"));
+        passwordPanel.add(passwordField);
+
+        JButton showPasswordButton = new JButton("Show Password");
+        showPasswordButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (!passwordVisible) {
+                    char[] passwordChars = passwordField.getPassword();
+                    String password = new String(passwordChars);
+                    passwordField.setEchoChar((char) 0);
+                    passwordField.setText(password);
+                    passwordVisible = true;
+                } else {
+                    passwordField.setEchoChar('\u25cf');
+                    passwordVisible = false;
+                }
+            }
+        });
+
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (username.equals("Marcus") && password.equals("ReserveMe!")) {
+                    openNewPage();
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Login Failed. Wrong username or password.");
+                }
+            }
+        });
+
+        rightPanel.add(usernamePanel);
+        rightPanel.add(passwordPanel);
+        rightPanel.add(showPasswordButton);
+        rightPanel.add(loginButton);
+
+        add(leftPanel);
+        add(rightPanel);
+
+        setVisible(true);
+    }
+
+    private void openNewPage() {
+        JFrame newPage = new JFrame("Main Menu");
+        newPage.setSize(800, 600);
+        newPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Yeni sayfayı üç sütuna bölmek için GridLayout kullan
+        newPage.setLayout(new GridLayout(1, 3));
+
+        // Her sütuna farklı arka plan resmi ve butonları ekleyin
+        JPanel panel1 = new JPanel(new BorderLayout());
+        panel1.add(new JLabel(new ImageIcon(System.getProperty("user.home") + "/Downloads/passenger.png")), BorderLayout.CENTER);
+        JButton passengerButton = new JButton("Passenger");
+        passengerButton.setAlignmentX(Component.CENTER_ALIGNMENT); // Butonları sütunun tam ortasına hizala
+        panel1.add(passengerButton, BorderLayout.SOUTH);
+        newPage.add(panel1);
+
+        JPanel panel2 = new JPanel(new BorderLayout());
+        panel2.add(new JLabel(new ImageIcon(System.getProperty("user.home") + "/Downloads/personnel.png")), BorderLayout.CENTER);
+        JButton personnelButton = new JButton("Personnel");
+        personnelButton.setAlignmentX(Component.CENTER_ALIGNMENT); // Butonları sütunun tam ortasına hizala
+        panel2.add(personnelButton, BorderLayout.SOUTH);
+        newPage.add(panel2);
+
+        JPanel panel3 = new JPanel(new BorderLayout());
+        panel3.add(new JLabel(new ImageIcon(System.getProperty("user.home") + "/Downloads/company.png")), BorderLayout.CENTER);
+        JButton companyButton = new JButton("Company");
+        companyButton.setAlignmentX(Component.CENTER_ALIGNMENT); // Butonları sütunun tam ortasına hizala
+        panel3.add(companyButton, BorderLayout.SOUTH);
+        newPage.add(panel3);
+
+          // Back ve Forward butonlarını ekleyin
+        JButton backButton = new JButton("Back");
+        JButton forwardButton = new JButton("Forward");
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(backButton);
+        buttonPanel.add(forwardButton);
+        newPage.add(buttonPanel);
+
+        newPage.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new UserPanel();
+        });
+    }
+}
